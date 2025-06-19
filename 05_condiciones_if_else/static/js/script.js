0e36189a8c4dff691c249fda84f8fffd3ac73baf
juandonoso1661 <juandonoso@liceovvh.cl>
2025-06-19 13:55:34 -0400

    a

--- 05_condiciones_if_else/static/js/script.js
+++ 05_condiciones_if_else/static/js/script.js
@@ -24,25 +24,102 @@ function edadVotar() {
  Pide una contraseña con prompt(). Si es igual a "1234"
  , muestra un mensaje de acceso permitido. En caso contrario, muestra acceso denegado.*/
 
-function validarContraseña(){
+function validarContraseña() {
     let clave = "1234"
     let pass = prompt("ingrese contraseña: ");
-//Condición estructura if - else
-if(pass == clave){
-    alert("Acceso permitido...")
-} else {
-    alert("Acceso denegado...")
-}
+    //Condición estructura if - else
+    if (pass == clave) {
+        alert("Acceso permitido...")
+    } else {
+        alert("Acceso denegado...")
+    }
 }
 
 /*Verificar par o impar
  Pide un número por prompt(). Evalúa si el número es par o impar y 
  muestra el resultado con alert().*/
 
- function ParImpar(){
-    let num = parseInt(prompt("ingrese num par:" ));
-if(){}
- }
+function parImpar() {
+    let numero = parseInt(prompt("ingrese num par:"));
+    if (numero % 2 == 0) {
+        alert("Es par");
+    } else {
+        alert("Es impar")
+    }
+}
+
+/*Temperatura ambiental
+ Solicita la temperatura actual. Si es mayor o igual a 30, muestra “Hace calor”, 
+ de lo contrario muestra “Clima agradable”.*/
+
+function temperaturaAmbiental() {
+    let temperatura = parseInt(prompt("Ingresar temperatura:  "));
+    if (temperatura >= 15 && temperatura <= 50) {
+        alert("Hace calor")
+    } else if (temperatura >= 15 && temperatura < 30) {
+        alert("Agradable")
+    } else if (temperatura <= -5 && temperatura < 15) {
+        alert("Hace frio")
+    } else {
+        alert("Ingrese un valor")
+    }
+};
+
+/*Comparar dos números
+Solicita dos números al usuario. Compara ambos y muestra cuál es mayor o si son iguales.*/
+
+function compararNumeros() {
+    let num1 = parseInt(prompt("Ingresar el primer número"));
+    let num2 = parseInt(prompt("Ingresar el segundo numero"));
+
+    if (num1 < num2) {
+        alert("el numero " + num1 + "es menor que " + num2);
+    } else if (num1 == num2) {
+        alert("el numero " + num1 + "es igual que " + num2);
+    } else {
+        alert("el numero " + num1 + "es mayor que " + num2);
+    }
+};
+
+/*Calificación escolar
+ Pide una nota entre 1 y 7. Si es 4 o más, 
+ muestra “Aprobado”, si es menor, muestra “Reprobado”.*/
+
+ function calificacionEscolar() {
+    let calificacion = parseInt(prompt("Ingrese su nota:  "));
+
+    if (calificacion < 4) {
+        alert("Reprovado")
+    } else if (calificacion >= 4 % calificacion <= 7) {
+        alert("Aprovado")
+    } else {
+        alert("Ingresar valor")
+    }
+ };
+
+ /*Nombre de usuario válido
+ Pide un nombre de usuario. Si el valor ingresado es
+  "admin", muestra “Bienvenido, administrador”. Si no, muestra “Usuario no reconocido”.*/
+
+  function comprobarUsuario() {
+    let usuario = prompt("Ingrese su usuario: ");
+    if (usuario == "admin"){
+        alert("Bienvenido, administrador..")
+    } else if (usuario !== "admin") {
+        alert("usuario no reconocido")
+    } else {
+        alert("Ingrese usuario")
+    }
+
+  }
+
+
+
+
+
+
+
+
 
 
 
